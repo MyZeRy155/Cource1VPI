2d36e614df46db9bb94aa67e8b16d711bbe9732a
СаняКоторыйНеВернетСотку <alexandrinyutin@gmail.com>
2018-11-14 23:58:06 +0300

    Добавил Task5

---
+++ PracticeWork4Task5/PracticeWork4Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PracticeWork4Task5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //5. Дано трехзначное число. В нем зачеркнули первую слева цифру и приписали ее в конце.
+            Console.WriteLine("Введите трехзначное число: ");
+            var A = Console.ReadLine();
+            var right = A.Substring(1);
+            var newNumber = right + A[0];
+            Console.WriteLine(newNumber);
+            Console.ReadLine();
+        }
+    }
+}
