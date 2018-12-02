a05871dd0f74e3c34f1d457858f94b7700932d2e
myzery <alexandrinyuti@gmail.com>
2018-12-02 21:51:29 +0300

    1

---
+++ Lab4/BonusTasks/ExtraTask1/ExtraTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExtraTask1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //1. Вычислить длину, площадь и объем окружности заданного радиуса.
+
+            Console.WriteLine("Введите радиус(R) : ");
+            var R = Convert.ToDouble(Console.ReadLine());
+           
+            var L = 2 * Math.PI * R;
+            var S = Math.PI * R * R;
+            var V = 4.0 / 3.0 * Math.PI * R * R * R;
+            
+            Console.WriteLine("Длинна равна  : " + L);
+            Console.WriteLine("Площадь равна : " + S);
+            Console.WriteLine("Обьем равен : " + V);
+            Console.ReadLine();
+        }
+    }
+}
