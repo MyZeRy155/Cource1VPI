8803450a17b94b8407e17b295ea06a98518788de
myzery <alexandrinyuti@gmail.com>
2018-12-18 22:15:20 +0300

    Добавил задание 1

---
+++ Lab6/PracticeWork6/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Напишите программу, выводящую на экран все четные числа от 1 до 100, подсчитывающую их количество и сумму.
+
+            int S = 0, K = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                Console.WriteLine(i);
+                K = K + 1;
+                S = S + i;
+                }
+            }
+            Console.WriteLine("S="+S);
+            Console.WriteLine("K="+K);
+            Console.ReadKey();
+        }
+    }
+}
